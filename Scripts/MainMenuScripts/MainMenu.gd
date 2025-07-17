3c3d9b1e422462b7df11f080e4323ba662ab0674
nh1570c <114521078+nh1570c@users.noreply.github.com>
2025-07-17 18:25:39 +0200

    EndScene hinzugefügt

--- Scripts/MainMenuScripts/MainMenu.gd
+++ Scripts/MainMenuScripts/MainMenu.gd
@@ -2,5 +2,11 @@ extends CanvasLayer
 
 @export var LevelScene = preload("res://Scenes/main.tscn")
 
+
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_packed(LevelScene)
+	var scene = get_tree().current_scene
+	print(scene)
+	if scene.name == "EndScene":  # or use scene.scene_file_path
+		get_tree().quit()  # Exit the game
+	else:
+		get_tree().change_scene_to_packed(LevelScene)
